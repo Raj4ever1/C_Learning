93e9cb58dd6ea979a8060a6717d7cc2bc8c292ce
Ronak Jain <rj.raj4ever@gmail.com>
2022-04-02 15:43:53 +0530

    Day 1 Starting

---
+++ 001.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+  printf("HELLO WORLD\nFuck You\n");
+  int a;
+  scanf("%d", &a);
+  printf("%d", a);
+  return 0;
+}
---
+++ 002.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+  int a=0,b=1,c=2;
+  printf("%d \n%d \n%d",a,b,c);
+  return 0;
+}
